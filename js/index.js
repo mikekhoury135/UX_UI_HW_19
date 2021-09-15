ecac4f38dadb2770c2683491c2e21ef91d078536
mikekhoury135 <mikekhoury135@gmail.com>
2021-09-15 07:51:41 -0400

    Test Commit
    Merging files from other repo to hand in HW19.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,58 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 100) {
-            $('.scrollToTop').fadeIn();
-        } else {
-            $('.scrollToTop').fadeOut();
-        }
+if ($(window).width() < 576) {
+    $(document).ready(function(){
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 400) {
+                $('.scrollToTop').fadeIn();
+            } else {
+                $('.scrollToTop').fadeOut();
+            }
+        });
+        $('.scrollToTop').click(function(){
+            $('html, body').animate({scrollTop : 0},100);
+            return false;        
+        });
+        $('.scroll-arrow').hover(function() {
+            $(this).animate({ margin: -10, width: "+=10", height: "+=10" });
+        }, function() {
+            $(this).animate({ margin: 0, width: "-=10", height: "-=10" });
+        });
     });
-    $('.scrollToTop').click(function(){
-        $('html, body').animate({scrollTop : 0},100);
-        return false;        
+} else if ($(window).width() < 992) {
+    $(document).ready(function(){
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 350) {
+                $('.scrollToTop').fadeIn();
+            } else {
+                $('.scrollToTop').fadeOut();
+            }
+        });
+        $('.scrollToTop').click(function(){
+            $('html, body').animate({scrollTop : 0},100);
+            return false;        
+        });
+        $('.scroll-arrow').hover(function() {
+            $(this).animate({ margin: -10, width: "+=10", height: "+=10" });
+        }, function() {
+            $(this).animate({ margin: 0, width: "-=10", height: "-=10" });
+        });
     });
-    $('.scroll-arrow').hover(function() {
-        $(this).animate({ margin: -10, width: "+=10", height: "+=10" });
-    }, function() {
-        $(this).animate({ margin: 0, width: "-=10", height: "-=10" });
+} else if ($(window).width() >= 992) {
+    $(document).ready(function(){
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 300) {
+                $('.scrollToTop').fadeIn();
+            } else {
+                $('.scrollToTop').fadeOut();
+            }
+        });
+        $('.scrollToTop').click(function(){
+            $('html, body').animate({scrollTop : 0},100);
+            return false;        
+        });
+        $('.scroll-arrow').hover(function() {
+            $(this).animate({ margin: -10, width: "+=10", height: "+=10" });
+        }, function() {
+            $(this).animate({ margin: 0, width: "-=10", height: "-=10" });
+        });
     });
-});
+};
